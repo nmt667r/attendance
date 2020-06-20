fc7fdd9d8439195d92bb5af08e319fcf3003642c
nmt667r <nm66flat@gmail.com>
2020-06-20 10:11:38 +0900

    XSS対策を実装

--- www/view/mypage_view.php
+++ www/view/mypage_view.php
@@ -6,7 +6,7 @@
 <body>
     <?php include_once '../view/template/header.php' ?>
     <div class="center">
-        <h2>ようこそ<?php echo $_SESSION['user_name']?>さん</h2>
+        <h2>ようこそ<?php echo h($_SESSION['user_name'])?>さん</h2>
 
         <?php already_come_check($db,'come','出勤')?>
         <?php already_come_check($db,'left','退勤')?>
